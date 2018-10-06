52ba72973854028e6f7329236a407e8b7e107291
Toshihiro Shimizu <shimizu@neuron.eei.eng.osaka-u.ac.jp>
2018-10-06 19:15:19 +0900

    2018/10/6

---
+++ 4-1/chap04/chap04/BaseDerivedTest1.cpp
@@ -0,0 +1,14 @@
+//// ”h¶‚Ìˆê—á
+//
+//#include <iostream>
+//#include "BaseDerived.h"
+//
+//using namespace std;
+//
+//int main()
+//{
+//	Derived dv(1, 2, 3);
+//
+//	cout << "dv.func()\n";		dv.func();		// Base‚©‚çŒp³‚µ‚½ƒƒ“ƒoŠÖ”
+//	cout << "dv.method()\n";	dv.method();	// Derived‚ÉŠ‘®‚·‚éƒƒ“ƒoŠÖ”
+//}
